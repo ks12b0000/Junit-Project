d35169bf54bd66bb87817ce6d90a8f12af9dec00
ks12b0000 <ks12b0000@gmail.com>
2022-10-11 18:52:31 +0900

    2022-10-11

--- src/main/java/site/metacoding/junitproject/util/MailSenderStub.java
+++ src/main/java/site/metacoding/junitproject/util/MailSenderStub.java
@@ -3,11 +3,11 @@ package site.metacoding.junitproject.util;
 import org.springframework.stereotype.Component;
 
 // 가짜
-@Component // IoC 컨테이너 등록
-public class MailSenderStub implements MailSender {
-
-    @Override
-    public boolean send() {
-        return true;
-    }
-}
+//@Component // IoC 컨테이너 등록
+//public class MailSenderStub implements MailSender {
+//
+//    @Override
+//    public boolean send() {
+//        return true;
+//    }
+//}
--- src/test/java/site/metacoding/junitproject/web/BookApiControllerTest.java
+++ src/test/java/site/metacoding/junitproject/web/BookApiControllerTest.java
@@ -1,5 +1,163 @@
 package site.metacoding.junitproject.web;
 
-// 통합 테스트
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+import org.springframework.test.context.jdbc.Sql;
+import site.metacoding.junitproject.domain.Book;
+import site.metacoding.junitproject.domain.BookRepository;
+import site.metacoding.junitproject.service.BookService;
+import site.metacoding.junitproject.web.dto.request.BookSaveReqDto;
+import site.metacoding.junitproject.web.dto.response.BookRespDto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+// 통합 테스트 (C,S,R)
+// 컨트롤러만 테스트하는 것이 아님.
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BookApiControllerTest {
+
+    @Autowired
+    private TestRestTemplate rt;
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    private static ObjectMapper om;
+    private static HttpHeaders headers;
+
+    @BeforeAll
+    public static void init() {
+        om = new ObjectMapper();
+        headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+    }
+
+    @BeforeEach // 각 테스트 시작전에 한번씩 실행됨.
+    public void 데이터준비() {
+        String title = "junit";
+        String author = "겟인데어";
+        Book book = Book.builder()
+                .title(title)
+                .author(author)
+                .build();
+        bookRepository.save(book);
+    }
+
+    @Test
+    public void saveBook_Test() throws Exception {
+        // given
+        BookSaveReqDto bookSaveReqDto = new BookSaveReqDto();
+        bookSaveReqDto.setTitle("스프링1강");
+        bookSaveReqDto.setAuthor("겟인데어");
+
+        String body = om.writeValueAsString(bookSaveReqDto);
+
+        // when
+        HttpEntity<String> request = new HttpEntity<>(body, headers);
+
+        ResponseEntity<String> response = rt.exchange("/api/v1/book", HttpMethod.POST, request, String.class);
+
+        System.out.println("response.getBody() = " + response.getBody());
+
+        // then
+        DocumentContext dc = JsonPath.parse(response.getBody());
+        String title = dc.read("$.body.title");
+        String author = dc.read("$.body.author");
+
+        assertThat(title).isEqualTo("스프링1강");
+        assertThat(author).isEqualTo("겟인데어");
+    }
+
+    @Sql("classpath:db/tableInit.sql")
+    @Test
+    public void getBookList() {
+        // given
+
+        // when
+        HttpEntity<String> request = new HttpEntity<>(null, headers);
+        ResponseEntity<String> response = rt.exchange("/api/v1/bookList", HttpMethod.GET, request, String.class);
+
+        System.out.println("response.getBody() = " + response.getBody());
+        // then
+        DocumentContext dc = JsonPath.parse(response.getBody());
+        Integer code = dc.read("$.code");
+        String title = dc.read("$.body.items[0].title");
+
+        assertThat(code).isEqualTo(1);
+        assertThat(title).isEqualTo("junit");
+    }
+
+    @Sql("classpath:db/tableInit.sql")
+    @Test
+    public void getBookOne() throws Exception {
+        // given
+        Long id = 1L;
+
+        // when
+        HttpEntity<String> request = new HttpEntity<>(null, headers);
+        ResponseEntity<String> response = rt.exchange("/api/v1/book/" + id, HttpMethod.GET, request, String.class);
+
+        System.out.println(response.getBody());
+
+        // then
+        DocumentContext dc = JsonPath.parse(response.getBody());
+        String title = dc.read("$.body.title");
+        String author = dc.read("$.body.author");
+
+        assertThat(title).isEqualTo("junit");
+        assertThat(author).isEqualTo("겟인데어");
+    }
+
+    @Sql("classpath:db/tableInit.sql")
+    @Test
+    public void deleteBook() {
+        // given
+        Long id = 1L;
+
+        // when
+        HttpEntity<String> request = new HttpEntity<>(null, headers);
+        ResponseEntity<String> response = rt.exchange("/api/v1/book/" + id, HttpMethod.DELETE, request, String.class);
+
+        // then
+        DocumentContext dc = JsonPath.parse(response.getBody());
+        Integer code = dc.read("$.code");
+        assertThat(code).isEqualTo(1);
+
+    }
+
+    @Sql("classpath:db/tableInit.sql")
+    @Test
+    public void updateBook() throws Exception{
+        // given
+        Long id = 1L;
+        BookSaveReqDto bookSaveReqDto = new BookSaveReqDto();
+        bookSaveReqDto.setTitle("스프링강의1");
+        bookSaveReqDto.setAuthor("메타코딩1");
+
+        String body = om.writeValueAsString(bookSaveReqDto);
+
+        // when
+        HttpEntity<String> request = new HttpEntity<>(body, headers);
+        ResponseEntity<String> response = rt.exchange("/api/v1/book" + id, HttpMethod.PUT, request, String.class);
+
+
+        // then
+//        System.out.println("response.getBody() = " + response.getStatusCodeValue());
+//        System.out.println("response.getBody() = " + response.getBody());
+        DocumentContext dc = JsonPath.parse(response.getBody());
+        String title = dc.read("$.body.title");
+
+        assertThat(title).isEqualTo("스프링강의1");
+    }
 }
